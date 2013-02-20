4bada8e5c3b598d2fb2c195bbdd767ba748ea6f4
Rick Parrish <github@rick.parrish>
2013-02-19 21:12:37 -0500

    Add class to ping highly available servers
    Which can be used to check whether computer is online or not

---
+++ AmIOnline.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandM.RMLib
+{
+    public static class AmIOnline
+    {
+        static private object _Lock = new object();
+        static private string[] _OnlineCheckHosts = { "akamai.com", "baidu.com", "blogspot.com", "cachefly.com", "cdnetworks.com", "cloudflare.com", "facebook.com", "gmail.com", "google.com", "hotmail.com", "linkedin.com", "qq.com", "twitter.com", "wikipedia.org", "wordpress.com", "yahoo.com", "youtube.com" };
+        static private int _OnlineCheckIndex = new Random().Next(0, _OnlineCheckHosts.Length);
+
+        public static bool PingNextHost()
+        {
+            int NextHostIndex = 0;
+
+            lock (_Lock)
+            {
+                _OnlineCheckIndex = (++_OnlineCheckIndex % _OnlineCheckHosts.Length);
+                NextHostIndex = _OnlineCheckIndex;
+            }
+
+            return WebUtils.Ping(_OnlineCheckHosts[NextHostIndex], 5000);
+        }
+
+        public static void CheckHosts()
+        {
+            foreach (string Host in _OnlineCheckHosts)
+            {
+                if (!WebUtils.Ping(Host, 5000))
+                {
+                    throw new Exception("Failed to ping " + Host);
+                }
+            }
+            throw new Exception("All hosts pingable");
+        }
+    }
+}
